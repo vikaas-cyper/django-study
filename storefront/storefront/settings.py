df5b2f8223ca42f83dc8bdab02561cf1f15f9ff6
vikaas-a <vikaas.cs20@bitsathy.ac.in>
2023-01-18 20:49:00 +0530

    Connect postgres with django

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
