2083dd86194fb5e211543443aaaf3c1d62de195a
vikaas-a <vikaas.cs20@bitsathy.ac.in>
2023-01-23 20:19:36 +0530

    select_related , prefetct_related

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from store.models import Product
+from store.models import OrderItem
+from store.models import Order
+from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # keyword = value
+    q = Order.objects.select_related('customer').prefetch_related(
+        'orderitem_set__product').order_by('id')
+    queryset = Product.objects.prefetch_related('promotions').order_by('title')
+    return render(request, 'hello.html', {'name': 'exits', 'products': list(q)})
