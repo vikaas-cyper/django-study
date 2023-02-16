f6a2e12704c6fbb8dca5ee99729e3a7d88e0b6c5
vikaas-a <vikaas.cs20@bitsathy.ac.in>
2023-02-16 10:46:37 +0530

    added new field

--- ssig/student/models.py
+++ ssig/student/models.py
@@ -21,6 +21,7 @@ class Student(models.Model):
     name = models.CharField(max_length=200)
     roll_no = models.CharField(max_length=20)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    batch = models.ForeignKey(Batch, on_delete= models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
