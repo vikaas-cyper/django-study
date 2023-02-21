f8410a49b28a342f27fda719f15f3ee876a6b2ef
vikaas-a <vikaas.cs20@bitsathy.ac.in>
2023-02-21 09:59:10 +0530

    Faculty Admin add created

--- ssig/faculty/admin.py
+++ ssig/faculty/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'faculty_id', 'department' , 'designation']
+
+
+@admin.register(models.Designation)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ['designation']
--- ssig/faculty/models.py
+++ ssig/faculty/models.py
@@ -6,7 +6,8 @@ class Designation(models.Model):
     designation = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    def __str__(self):
+        return self.designation
 class Faculty(models.Model):
     name = models.CharField(max_length=200)
     faculty_id = models.CharField(max_length=20)
